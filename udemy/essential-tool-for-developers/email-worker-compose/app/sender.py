46508c5f8356c75214ef7c58c59bdf20989c74a7
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-09-08 14:08:46 -0300

    61. Networks

--- udemy/essential-tool-for-developers/email-worker-compose/app/sender.py
+++ udemy/essential-tool-for-developers/email-worker-compose/app/sender.py
@@ -1,10 +1,28 @@
+import psycopg2
 from bottle import route, request, run
+
+
+DSN = 'dbname=email_sender user=postgres host=db'
+SQL = 'INSERT INTO emails (subject, message) VALUES (%s, %s)'
+
+
+def register_message(subject, message):
+    conn = psycopg2.connect(DSN)
+    cur = conn.cursor()
+    cur.execute(SQL, (subject, message))
+    conn.commit()
+    cur.close()
+    conn.close()
+    print('####################################### Message registered! #######################################')
 
 
 @route('/', method='POST')
 def send():
     subject = request.forms.get('subject')
     message = request.forms.get('message')
+
+    register_message(subject, message)
+
     return f'Message queued! Subject: {subject} - Message: {message}'
 
 
--- udemy/essential-tool-for-developers/email-worker-compose/scripts/init.sql
+++ udemy/essential-tool-for-developers/email-worker-compose/scripts/init.sql
@@ -5,7 +5,6 @@ create database email_sender;
 create table emails (
     id serial not null primary key,
     date timestamp not null default now(),
-    email varchar(255) not null,
     subject varchar(255) not null,
     message varchar(255) not null
 );
