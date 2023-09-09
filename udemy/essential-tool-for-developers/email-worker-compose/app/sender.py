63da5c1b55f2912e21fc5723c606c998a2b1f9b9
Joao.Nogueira <jprnogueira@yahoo.com.br>
2023-09-08 22:47:48 -0300

    64. Good practices — Environment variables

--- udemy/essential-tool-for-developers/email-worker-compose/app/sender.py
+++ udemy/essential-tool-for-developers/email-worker-compose/app/sender.py
@@ -2,9 +2,8 @@ import psycopg2
 import redis
 import json
 from bottle import Bottle, request
+import os
 
-
-DSN = 'dbname=email_sender user=postgres host=db'
 SQL = 'INSERT INTO emails (subject, message) VALUES (%s, %s)'
 
 
@@ -12,8 +11,15 @@ class Sender(Bottle):
     def __init__(self):
         super().__init__()
         self.route('/', method='POST', callback=self.send)
-        self.row = redis.StrictRedis(host='queue', port=6379, db=0)
-        self.conn = psycopg2.connect(DSN)
+
+        redis_host = os.environ.get('REDIS_HOST', 'queue')
+        self.row = redis.StrictRedis(host=redis_host, port=6379, db=0)
+
+        db_host = os.environ.get('DB_HOST', 'db')
+        db_user = os.environ.get('DB_USER', 'postgres')
+        db_name = os.environ.get('DB_NAME', 'email_sender')
+        dsn = f'dbname={db_name} user={db_user} host={db_host}'
+        self.conn = psycopg2.connect(dsn)
 
     def register_message(self, subject, message):
         cur = self.conn.cursor()
--- udemy/essential-tool-for-developers/email-worker-compose/worker/worker.py
+++ udemy/essential-tool-for-developers/email-worker-compose/worker/worker.py
@@ -1,11 +1,13 @@
 import redis
 import json
+import os
 from time import sleep
 from random import randint
 
 
 if __name__ == '__main__':
-    r = redis.StrictRedis(host='queue', port=6379, db=0)
+    redis_host = os.environ.get('REDIS_HOST', 'queue')
+    r = redis.StrictRedis(host=redis_host, port=6379, db=0)
     print('####################################### Worker started! #######################################')
     while True:
         message = json.loads(r.blpop('sender')[1])
